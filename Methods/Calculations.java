52d2fbe2d398fc8f83fe11a8a3c3dd1fe8fe28cf
EmilyRAHS <43458479+EmilyRAHS@users.noreply.github.com>
2021-10-19 00:03:42 -0700

    test

---
+++ Methods/Calculations.java
@@ -0,0 +1,27 @@
+public class Calculations {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printHi() {
+        System.out.println("Hi");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
+    // adds 3 to the number entered
+    public static int add(int a) {
+        return add(a, 3);
+    }
+
+    // adds the two numbers inputted as 
+    public static int add(int a, int b) {
+        
+    }
+
+    public static int subtract(int a, int b) {
+
+    }
+}
